55a9ea0e46f628c387a78e063fcc9911d5c7672e
larcondo <larcondo90@gmail.com>
2023-08-31 18:40:21 -0300

    synchronizing data to the server using React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useQuery } from '@tanstack/react-query'
-import { getNotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests'
 
 function App() {
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
-    event.target.note.value
-    console.log(content)
+    event.target.note.value = ''
+    newNoteMutation.mutate({ content, important: true })
   }
 
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important })
   }
 
   const result = useQuery({
